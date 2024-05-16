8f81a7ff43b8dd02c937abbd41e29d8e57b4708a
PatrickDalington <patrickdalington11@gmail.com>
2024-05-16 21:38:52 +0100

    (fix) Login Page

---
+++ src/app/login/page.jsx
@@ -0,0 +1,11 @@
+import styles from './login.module.css'
+
+const Login = () => {
+  return (
+    <div className={styles.container}>
+      <div>Login Now!</div>
+    </div>
+  )
+}
+
+export default Login
